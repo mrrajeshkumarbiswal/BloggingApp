0d4dfe7944e3cbcadd14ce7e9cde248fc975cc27
raj <rajeshkumarbiswal5@gmail.com>
2024-07-25 02:47:23 +0530

    first commit

---
+++ src/test/java/kumar/BlogApp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kumar.BlogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
